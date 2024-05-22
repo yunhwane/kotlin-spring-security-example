c10e5c3147534e4559fc414e3a2cad9db4494a82
wjsdbsghks96@dgu.ac.kr <wjsdbsghks96@dgu.ac.kr>
2024-05-22 20:46:32 +0900

    first commit

---
+++ src/main/kotlin/com/example/testcode/TestcodeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.testcode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestcodeApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestcodeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/testcode/TestcodeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.testcode
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestcodeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
